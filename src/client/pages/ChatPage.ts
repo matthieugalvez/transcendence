8035f1a95d774377bfdcf5783d93b43f143e0235
Matthieu Galvez <mgalvez@f1br1s3.clusters.42paris.fr>
2025-06-19 15:00:36 +0200

    chat page WIP 2

--- src/client/pages/ChatPage.ts
+++ src/client/pages/ChatPage.ts
@@ -1,6 +1,6 @@
 import '../styles.css';
 import { BackgroundComponent } from '../components/background.component';
-//import { CommonComponent } from '../components/common.component';
+import { CommonComponent } from '../components/common.component';
 import { ChatService } from '../services/chat.service';
 import { UserService } from '../services/user.service';
 
@@ -18,7 +18,7 @@ export async function renderChatPage() {
 		text-white font-semibold
 		border-2 border-black
         fixed left-[5%] top-2 h-[5%] w-[90%]
-        bg-blue-950/70 backdrop-blur-2xl
+        bg-amber-300/50
         rounded-lg text-lg transition-colors
         focus:outline-none focus:ring-2
         shadow-[4.0px_5.0px_0.0px_rgba(0,0,0,0.8)]
@@ -29,18 +29,20 @@ export async function renderChatPage() {
 	`.trim();
 	title_box.textContent = `Chat room between ${user.displayName} & ${receiver.displayName}`
 
-	ChatService.postMessage(user.id, 'test');
+	document.body.appendChild(title_box);
+
+//	ChatService.postMessage(user.id, 'test');
 //	ChatService.editMessage(1, 'edit test');
 	let		messages = await ChatService.getAllMessages();
-	if (messages.length > 6)
+	while (messages.length > 9)
 	{
 		await ChatService.deleteMessage(messages[0].id);
 		messages = await ChatService.getAllMessages();
 	}
 	const	send_box = document.createElement('nav');
 	send_box.className = `
-        fixed left-10 top-20 h-[75%] w-[45%]
-        bg-blue-950/70 backdrop-blur-2xl
+        fixed left-[2.5%] top-[7%] h-[65%] w-[46%]
+        bg-amber-300/50
         rounded-lg text-lg transition-colors
         focus:outline-none focus:ring-2
         shadow-[4.0px_5.0px_0.0px_rgba(0,0,0,0.8)]
@@ -52,8 +54,8 @@ export async function renderChatPage() {
 
 	const	received_box = document.createElement('nav');
 	received_box.className = `
-        fixed right-10 top-20 h-[75%] w-[45%]
-        bg-blue-950/70 backdrop-blur-2xl
+        fixed right-[2.5%] top-[7%] h-[65%] w-[46%]
+        bg-amber-300/50
         rounded-lg text-lg transition-colors
         focus:outline-none focus:ring-2
         shadow-[4.0px_5.0px_0.0px_rgba(0,0,0,0.8)]
@@ -70,16 +72,63 @@ export async function renderChatPage() {
 			makeMsgBox(received_box, message);
 	}
 
-	document.body.appendChild(title_box);
 	document.body.appendChild(send_box);
 	document.body.appendChild(received_box);
+
+	const	prompt_box = document.createElement('div');
+	prompt_box.className = `
+        fixed bottom-5 h-[25%] w-[99%]
+        bg-amber-300/50
+        rounded-lg text-lg transition-colors
+        focus:outline-none focus:ring-2
+        shadow-[4.0px_5.0px_0.0px_rgba(0,0,0,0.8)]
+        disabled:opacity-50 disabled:cursor-not-allowed
+        border-2 border-black
+        flex flex-col items-start p-6
+        space-y-4 z-11
+	`.trim()
+
+	const	prompt_title = document.createElement('label');
+	prompt_title.className = `
+    font-['Orbitron']
+    text-white font-semibold
+  `.trim();
+	prompt_title.textContent = 'New message:';
+
+	const	prompt_area = document.createElement('textarea');
+	prompt_area.className = `
+    font-['Orbitron']
+    bg-purple-900/100 backdrop-blur-2xl
+    h-[65%] w-[100%]
+    text-white font-semibold p-1
+    border-2 border-black
+    rounded-lg text-lg transition-colors
+    focus:outline-none focus:ring-2
+    shadow-[4.0px_5.0px_0.0px_rgba(0,0,0,0.8)]
+    disabled:opacity-50 disabled:cursor-not-allowed
+	resize-none
+	`.replace(/\s+/g, ' ').trim()
+	prompt_area.placeholder = 'Type Message here...';
+
+	const	send_button = CommonComponent.createStylizedButton("Send", 'blue');
+	send_button.style.position = 'inherit';
+	send_button.style.right = '35px';
+	send_button.style.bottom = '35px';
+//	send_button.onclick();
+
+	prompt_box.appendChild(prompt_title);
+	prompt_box.appendChild(prompt_area);
+	prompt_box.appendChild(send_button);
+	document.body.appendChild(prompt_box);
 }
 
 function makeMsgBox(content_box: Element, message) {
 	const	send_box_date = document.createElement('nav');
 	send_box_date.className = `
     font-['Orbitron']
+	relative top-[-70px] left-2
     text-white font-semibold
+    bg-purple-900/50 backdrop-blur-2xl
     border-2 border-black
     rounded-lg text-lg transition-colors
     focus:outline-none focus:ring-2
@@ -87,28 +136,33 @@ function makeMsgBox(content_box: Element, message) {
     disabled:opacity-50 disabled:cursor-not-allowed
     flex flex-col items-center justify-center
   `.replace(/\s+/g, ' ').trim();
-//	send_box_date.style.whiteSpace = 'pre-line';
 	send_box_date.style.fontSize = '14px';
-	send_box_date.style.margin = '2px';
 	send_box_date.textContent = `
-		at: ${message.created_at}
+		${message.created_at}
 	`;
-	content_box.appendChild(send_box_date);
 
 	const	send_box_content = document.createElement('nav');
 	send_box_content.className = `
     font-['Orbitron']
+    bg-purple-900/100 backdrop-blur-2xl
     left-10 w-[100%]
-    text-white font-semibold
+    text-white font-semibold p-1
     border-2 border-black
     rounded-lg text-lg transition-colors
     focus:outline-none focus:ring-2
     shadow-[4.0px_5.0px_0.0px_rgba(0,0,0,0.8)]
     disabled:opacity-50 disabled:cursor-not-allowed
 	`.replace(/\s+/g, ' ').trim();
-	send_box_content.style.whiteSpace = 'pre';
+	send_box_content.style.whiteSpace = 'pre-line';
+	send_box_date.style.margin = '-20px';
 	send_box_content.textContent = `
 		${message.content}
 	`;
+
+	const	margin = document.createElement('div');
+	margin.style.margin = '10px';
+
 	content_box.appendChild(send_box_content);
+	content_box.appendChild(send_box_date);
+	content_box.appendChild(margin);
 }
