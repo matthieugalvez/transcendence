9bfda19b133dc67403d706684f098aaddda22893
Emma Blanchard <eblancha@f1br8s18.clusters.42paris.fr>
2025-06-26 16:58:24 +0200

    little fix tournament local

--- src/client/pages/TournamentPage.ts
+++ src/client/pages/TournamentPage.ts
@@ -117,26 +117,9 @@ export async function launchTournament(aliases: string[], wrapper: HTMLElement) 
     const pongHandle = startPongInContainer(
       gameContainer, matchTitle, leftAlias, rightAlias,
       async (winnerId, score1, score2) => {
-        // const p1 = await UserService.getUserProfileByDisplayName(leftAlias);
-        // const p2 = await UserService.getUserProfileByDisplayName(rightAlias);
-        const p1 = participants.find(p => p.alias === leftAlias)!;
-        const p2 = participants.find(p => p.alias === rightAlias)!;
+        const p1 = await UserService.getUserProfileByDisplayName(leftAlias);
+        const p2 = await UserService.getUserProfileByDisplayName(rightAlias);
         const winnerAliasId = winnerId === 1 ? p1.id : p2.id;
-        // if (!aliasesIdArray.length) {
-        //   aliasesIdArray = [
-        //     ...(await Promise.all(aliases.map(a =>
-        //       UserService.getUserProfileByDisplayName(a).then(u => u.id)
-        //     )))
-        //   ]
-        // }
-        // await GameService.createMatch(gameId, {
-        //   playerOneId: p1.id,
-        //   playerTwoId: p2.id,
-        //   winnerId: winnerId === 1 ? p1.id : p2.id,
-        //   matchType: 'TOURNAMENT',
-        //   playerOneScore: score1,
-        //   playerTwoScore: score2
-        // });
         playedMatches.push({
           playerOneId: p1.id,
           playerTwoId: p2.id,
